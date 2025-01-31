9e90cafe41a64c8dd7837551ce952935bfb91fa6
yokxim <yooksi53@gmail.com>
2025-02-01 00:15:27 +0900

    feat[custom-user-details-service] UserDetailsService 상속받는 커스텀 서비스 클래스 생성
    - DB에서 username으로 입력 받은 유저명 검색 -> 유저 찾기 -> CustomUserDetails 객체 생성

---
+++ src/main/java/org/example/deepsecurity/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package org.example.deepsecurity.service;
+
+import org.example.deepsecurity.dto.CustomUserDetails;
+import org.example.deepsecurity.entity.UserEntity;
+import org.example.deepsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+            return new CustomUserDetails(userData);
+        }
+        return null;
+    }
+}

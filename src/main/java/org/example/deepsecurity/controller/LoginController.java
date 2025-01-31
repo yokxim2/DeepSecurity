71c50946df513acf2c70d12a89306f20f5d9308f
yokxim <yooksi53@gmail.com>
2025-01-31 23:04:48 +0900

    feat[login-controller] 로그인 url을 처리하는 컨트롤러 추가

---
+++ src/main/java/org/example/deepsecurity/controller/LoginController.java
@@ -0,0 +1,13 @@
+package org.example.deepsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginP() {
+        return "login";
+    }
+}

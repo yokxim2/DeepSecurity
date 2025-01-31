275ec66918c97549139788ce16f8ba36df6cf0f0
yokxim <yooksi53@gmail.com>
2025-01-31 23:47:36 +0900

    feat[join-service] 회원가입 처리 서비스 클래스 추가

---
+++ src/main/java/org/example/deepsecurity/service/JoinService.java
@@ -0,0 +1,30 @@
+package org.example.deepsecurity.service;
+
+import org.example.deepsecurity.dto.JoinDTO;
+import org.example.deepsecurity.entity.UserEntity;
+import org.example.deepsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void joinProcess(JoinDTO joinDTO) {
+
+
+
+        UserEntity data = new UserEntity();
+        data.setUsername(joinDTO.getUsername());
+        data.setPassword(bCryptPasswordEncoder.encode(joinDTO.getPassword()));
+        data.setRole("ROLE_USER");
+
+        userRepository.save(data);
+    }
+}

6a92ec6e9bacceb7853db47f86c37b9cbe91eee4
yokxim <yooksi53@gmail.com>
2025-01-31 23:48:47 +0900

    fix[security-config] 추가한 회원가입 로직 검증을 안할 수 있도록 url 추가

--- src/main/java/org/example/deepsecurity/config/SecurityConfig.java
+++ src/main/java/org/example/deepsecurity/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "login").permitAll()
+                        .requestMatchers("/", "/login", "/join", "/joinProc").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated()

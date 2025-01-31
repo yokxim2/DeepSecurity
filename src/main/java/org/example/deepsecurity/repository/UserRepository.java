ec453144263d99cb0d6e156270fc1d5b6f5b617c
yokxim <yooksi53@gmail.com>
2025-02-01 00:16:10 +0900

    fix[user-repository] 유저 명 검색 메서드 추가

--- src/main/java/org/example/deepsecurity/repository/UserRepository.java
+++ src/main/java/org/example/deepsecurity/repository/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
     boolean existsByUsername(String username);
+    UserEntity findByUsername(String username);
 }
